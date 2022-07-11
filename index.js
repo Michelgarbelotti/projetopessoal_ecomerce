102413b6779c98a18a0194ba4c41cdc59af6e657
Michel Garbelotti <michelgarbelotti@gmail.com>
2022-07-11 19:32:42 -0300

    atualização

--- index.js
+++ index.js
@@ -12,13 +12,20 @@ function itemSelecionado1() {
   document.getElementById("antigo-preco").innerHTML = "R$ 18,00";
   document.getElementById("preco-atual").innerHTML = "R$ 16,00";
   document.getElementById("parcelado").innerHTML = "R$ 5,33 ";
+
+  limpar();
+  document.getElementById("btn-quantidade1").classList.add("btn-selecionado");
 }
 
 function itemSelecionado2() {
+  
   document.getElementById("preco-descontado").innerHTML = "R$ 28,50";
   document.getElementById("antigo-preco").innerHTML = "R$ 32,00";
   document.getElementById("preco-atual").innerHTML = "R$ 30,00";
   document.getElementById("parcelado").innerHTML = "R$ 10,00 ";
+
+  limpar();
+  document.getElementById("btn-quantidade2").classList.add("btn-selecionado");
 }
 
 function itemSelecionado3() {
@@ -26,6 +33,9 @@ function itemSelecionado3() {
   document.getElementById("antigo-preco").innerHTML = "R$ 67,00";
   document.getElementById("preco-atual").innerHTML = "R$ 65,00";
   document.getElementById("parcelado").innerHTML = "R$ 21,66 ";
+
+  limpar();
+  document.getElementById("btn-quantidade3").classList.add("btn-selecionado");
 }
 
 function itemSelecionado4() {
@@ -33,8 +43,19 @@ function itemSelecionado4() {
   document.getElementById("antigo-preco").innerHTML = "R$ 120,00";
   document.getElementById("preco-atual").innerHTML = "R$ 115,00";
   document.getElementById("parcelado").innerHTML = "R$ 38,33 ";
+
+  limpar();
+  document.getElementById("btn-quantidade4").classList.add("btn-selecionado");
 }
 
+function limpar() {
+  document.getElementById("btn-quantidade1").classList.remove("btn-selecionado");
+  document.getElementById("btn-quantidade2").classList.remove("btn-selecionado");
+  document.getElementById("btn-quantidade3").classList.remove("btn-selecionado");
+  document.getElementById("btn-quantidade4").classList.remove("btn-selecionado");
+ 
+
+}
 
 // função de quantidade do produto // 
 function process(quant) {
